70cf3a96509ed956a0b646eba318c1445688c0e8
apavlova <anastasiya.pavlova@emergn.com>
2021-11-10 00:54:07 +0300

    tech/#5-add-database-connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports    : [ MongooseModule.forRoot('mongodb://localhost:27017/tripleR') ],
+  controllers: [ AppController ],
+  providers  : [ AppService ],
 })
 export class AppModule {}
